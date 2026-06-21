ab072bf6213c40603d4785c16ab98d4a88d87480
Arnav Vaidya <arnavaidya@gmail.com>
2026-06-21 18:11:14 +0530

    Solution 2

--- 2.Random_Password_Generator.py
+++ 2.Random_Password_Generator.py
@@ -1,5 +1,18 @@
-def rand_pass_gen(pass_len = 8):
+import string
+import random
 
+def rand_pass_gen(length = 8):
 
-pass_len = input("Enter the length of password (default = 8): ")
-rand_pass_gen(pass_len)
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(characters) for i in range(length))
+    return password
+
+pass_len_str = input("Enter the length of password (default: 8 characters): ")
+
+if pass_len_str:
+    length = int(pass_len_str)
+else:
+    length = 8
+
+password = rand_pass_gen(length)
+print("Generated password: " + password)
